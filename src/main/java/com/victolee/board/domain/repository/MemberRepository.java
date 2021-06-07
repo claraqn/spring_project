1103afe3e5fbbb9198e10b9bdc7231c262a6c48f
unknown <claraqn@naver.com>
2021-06-07 20:33:34 +0900

    0607

---
+++ src/main/java/com/victolee/board/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.victolee.board.domain.repository;
+
+import com.victolee.board.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
